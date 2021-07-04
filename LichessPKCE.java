c586ad3298a4ef5a0f23ee4b2314b5547d379b6f
tors42 <4084220+tors42@users.noreply.github.com>
2021-07-04 11:29:58 +0200

    Refactoring
    - Divide main() into three steps
      1. login
      2. read email
      3. logout
    
    - Less noisy
      Only output on unexpected stuff
    
    - Better use of "state" parameter
      Generate the state parameter, instead of hardcoded
      Verify that same state parameter is returned in response
    
    - Update/add some clarifying comments

--- LichessPKCE.java
+++ LichessPKCE.java
@@ -20,6 +20,8 @@ import com.sun.net.httpserver.HttpServer;
 
 public class LichessPKCE {
 
+    public static String lichessUri = "https://lichess.org";
+
     /**
      * This demo application will launch a Web Browser,
      * where authentication with Lichess can be made,
@@ -30,7 +32,25 @@ public class LichessPKCE {
      */
     public static void main(String... args) throws Exception {
 
-        var lichessUri = "https://lichess.org";
+        // Perform the OAuth2 PKCE flow
+        var access_token = login();
+
+        // Fetch the e-mail address
+        var email = readEmail(access_token);
+
+        System.out.println("e-mail: " + email);
+
+        // Logout
+        logout(access_token);
+
+    }
+
+    static String login() throws Exception {
+
+        // Prepare a new login.
+        // We will generate a lot of parameters which will be used in this login,
+        // and then the parameters are thrown away, not to be re-used.
+        // I.e, next login request will have new parameters generated.
 
         // Setup a local bind address which we will use in redirect_url
         var local = new InetSocketAddress(InetAddress.getLoopbackAddress(), 0);
@@ -46,7 +66,7 @@ public class LichessPKCE {
         var client_id = "apptest";
         var redirect_uri = "http://" + redirectHost + ":" + redirectPort;
         var scope = "email:read";
-        var state = "foobar";
+        var state = generateRandomState();
 
         var parameters = Map.of(
                 "code_challenge_method", code_challenge_method,
@@ -62,17 +82,19 @@ public class LichessPKCE {
             .map(kv -> kv.getKey() + "=" + kv.getValue())
             .collect(Collectors.joining("&"));
 
+        // Front Channel URL, all these parameters are non-sensitive.
+        // The actual authentication between User and Lichess happens outside of this demo application,");
+        // i.e in the browser over HTTPS.");
         var frontChannelUrl = URI.create(lichessUri +"/oauth" + "?" + paramString);
-        System.out.println("Front Channel URL, all these parameters are non-sensitive:\n" + frontChannelUrl);
-        System.out.println("The actual authentication between User and Lichess happens outside of this demo application,");
-        System.out.println("i.e in the browser over HTTPS.");
 
         // Prepare for handling the upcoming redirect,
         // after User has authenticated with Lichess,
         // and granted this demo application permission
         // to fetch the e-mail address.
-        var cf = registerRedirectHandler(httpServer, lichessUri, code_verifier, redirect_uri, client_id);
-
+        // The random code_verifier we generated for this single login,
+        // will be sent to Lichess on a "Back Channel" so they can verify that
+        // the Front Channel request really came from us.
+        var cf = registerRedirectHandler(httpServer, parameters, code_verifier);
 
         // Now we let the User authorize with Lichess,
         // using their browser
@@ -97,36 +119,48 @@ public class LichessPKCE {
         // and we've exchanged the incoming authorization code for an access token
         var access_token = cf.get();
 
-        // Authorization Flow Complete
+        httpServer.stop(0);
+
+        return access_token;
+    }
+
+    static String readEmail(String access_token) throws Exception {
 
         // Get that e-mail
-        var httpClient = HttpClient.newHttpClient();
         var emailRequest = HttpRequest.newBuilder(URI.create(lichessUri + "/api/account/email"))
             .GET()
             .header("authorization", "Bearer " + access_token)
             .header("accept", "application/json")
             .build();
 
-        var response = httpClient.send(emailRequest, BodyHandlers.ofString());
-
+        var response = HttpClient.newHttpClient().send(emailRequest, BodyHandlers.ofString());
         var statusCode = response.statusCode();
         var body = response.body();
         var email = parseField("email", body);
+        if (statusCode != 200) {
+            System.out.println("/api/account/email - " + statusCode);
+        }
+        return email;
 
-        System.out.println("/api/account/email - " + statusCode);
-        System.out.println("e-mail: " + email);
+    }
 
-        httpServer.stop(0);
+    static void logout(String access_token) throws Exception {
+        var logoutRequest = HttpRequest.newBuilder(URI.create(lichessUri + "/api/token"))
+            .DELETE()
+            .header("authorization", "Bearer " + access_token)
+            .build();
+
+        var response = HttpClient.newHttpClient().send(logoutRequest, BodyHandlers.discarding());
+        var statusCode = response.statusCode();
+        if (statusCode != 204) {
+            System.out.println("/api/token - " + response.statusCode());
+        }
     }
 
     static String generateRandomCodeVerifier() {
         var bytes = new byte[32];
         new Random().nextBytes(bytes);
-        var code_verifier = Base64.getUrlEncoder().encodeToString(bytes)
-            .replaceAll(  "=",  "")
-            .replaceAll("\\+", "-")
-            .replaceAll("\\/", "_");
-
+        var code_verifier = encodeToString(bytes);
         return code_verifier;
     }
 
@@ -141,14 +175,27 @@ public class LichessPKCE {
         }
 
         var s256bytes = md.digest(asciiBytes);
-        var code_challenge = Base64.getUrlEncoder().encodeToString(s256bytes)
-            .replaceAll(  "=",  "")
-            .replaceAll("\\+", "-")
-            .replaceAll("\\/", "_");
+
+        var code_challenge = encodeToString(s256bytes);
         return code_challenge;
     }
 
-    static CompletableFuture<String> registerRedirectHandler(HttpServer httpServer, String lichessUri, String code_verifier, String redirect_uri, String client_id) {
+    static String generateRandomState() {
+        var bytes = new byte[16];
+        new Random().nextBytes(bytes);
+        // Not sure how long the parameter "should" be,
+        // going for 8 characters here...
+        return encodeToString(bytes).substring(0,8);
+    }
+
+    static String encodeToString(byte[] bytes) {
+         return Base64.getUrlEncoder().encodeToString(bytes)
+            .replaceAll(  "=",  "")
+            .replaceAll("\\+", "-")
+            .replaceAll("\\/", "_");
+    }
+
+    static CompletableFuture<String> registerRedirectHandler(HttpServer httpServer, Map<String, String> requestParams, String code_verifier) {
         var cf = new CompletableFuture<String>();
         httpServer.createContext("/",
                 (exchange) -> {
@@ -166,6 +213,11 @@ public class LichessPKCE {
                                     ));
 
                     var code = inparams.get("code");
+                    var state = inparams.get("state");
+
+                    if (! state.equals(requestParams.get("state"))) {
+                        throw new RuntimeException("The \"state\" parameter we sent and the one we recieved didn't match!");
+                    }
 
                     if (code == null) {
                         exchange.sendResponseHeaders(503, -1);
@@ -188,26 +240,27 @@ public class LichessPKCE {
                             "code_verifier", code_verifier,
                             "grant_type", "authorization_code",
                             "code", code,
-                            "redirect_uri", redirect_uri,
-                            "client_id", client_id
+                            "redirect_uri", requestParams.get("redirect_uri"),
+                            "client_id", requestParams.get("client_id")
                             );
 
                     var tokenParamsString = tokenParameters.entrySet().stream()
                         .map(kv -> kv.getKey() + "=" + kv.getValue())
                         .collect(Collectors.joining("&"));
 
-                    var httpClient = HttpClient.newHttpClient();
                     var tokenRequest = HttpRequest.newBuilder(URI.create(lichessUri + "/api/token"))
                         .POST(BodyPublishers.ofString(tokenParamsString))
                         .header("content-type", "application/x-www-form-urlencoded")
                         .build();
 
                     try {
-                        var response = httpClient.send(tokenRequest, BodyHandlers.ofString());
+                        var response = HttpClient.newHttpClient().send(tokenRequest, BodyHandlers.ofString());
                         var statusCode = response.statusCode();
                         var body = response.body();
 
-                        System.out.println("/api/token - " + statusCode);
+                        if (statusCode != 200) {
+                            System.out.println("/api/token - " + statusCode);
+                        }
                         var access_token = parseField("access_token", body);
 
                         if (access_token == null) {
